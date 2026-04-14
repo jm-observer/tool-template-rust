fb7559ad5c4f521f87c6d903052c48bc493091e8
fengqi <362254883@qq.com>
2026-04-14 11:38:44 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use log::LevelFilter::Info;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let _ = logger::logger_feature("app", "debug", Info, false).build();
+    let _ = custom_utils::daemon::daemon();
     // remember to print msg via stdio
-    rust_template::run().await
+    tool::run().await
 }
